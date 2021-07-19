4c45e7503a2b9bbfc160ce3c396411efd2fa2eeb
Dheep Shemare <dheep@computors.com>
2021-07-19 11:34:53 +0100

    Small changes

--- js/test.js
+++
@@ -1,9 +0,0 @@
-
-function makeList(){
-
-    let item = document.getElementById("listInput").value
-    let text = document.createTextNode(item)
-    let newItem = document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("dynamicList").appendChild(newItem)
-}
